b64512b69b871f6f1f355f2feab55b9871de6884
vikram <thevikrampanwar@gmail.com>
2019-01-31 11:50:02 +0530

    commited hello git project

---
+++ Array/src/com/Prints.java
@@ -0,0 +1,12 @@
+package com;
+
+public class Prints {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] names= {"vikram","kiky","pallu","diler"};
+		for(String s:names)
+		System.out.println(s);
+	}
+
+}
---
+++ Array/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author vikram
+ *
+ */
+module Array {
+}
